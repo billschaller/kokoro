5d5f5fae5072307bedfbf32d6bb791dfda9fa224
Bill Schaller <wschalle+billschaller@gmail.com>
2025-06-09 07:48:25 -0400

    Make XPU work

--- kokoro/pipeline.py
+++ kokoro/pipeline.py
@@ -96,6 +96,8 @@ class KPipeline:
                 raise RuntimeError("CUDA requested but not available")
             if device == 'mps' and not torch.backends.mps.is_available():
                 raise RuntimeError("MPS requested but not available")
+            if device == 'xpu' and not torch.xpu.is_available():
+                raise RuntimeError("XPU requested but not available")
             if device == 'mps' and os.environ.get('PYTORCH_ENABLE_MPS_FALLBACK') != '1':
                 raise RuntimeError("MPS requested but fallback not enabled")
             if device is None:
@@ -103,6 +105,8 @@ class KPipeline:
                     device = 'cuda'
                 elif os.environ.get('PYTORCH_ENABLE_MPS_FALLBACK') == '1' and torch.backends.mps.is_available():
                     device = 'mps'
+                elif torch.xpu.is_available():
+                    device = 'xpu'
                 else:
                     device = 'cpu'
             try:
